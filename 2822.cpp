31bdff7b4a24acb393aea4276398a67c3eac7fcd
Croissanttttt <joyson1125@hanmail.net>
2019-11-05 13:42:28 +0900

    Desktop first

---
+++ 2822.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void){
+	int i, sum;
+	int score[7];
+	for(i=0;i<8;i++){scanf("%d",&score[i]);}
+	sum=score[0]+score[1]+score[2]+score[3]+score[4];
+	if(score)
+	printf("%d",sum);
+	return 0;
+}
