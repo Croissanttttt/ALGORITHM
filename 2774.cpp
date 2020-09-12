2121e1002eaee5c4b179f5e6f9664ff20d65595f
Croissanttttt <joyson1125@hanmail.net>
2020-09-12 17:06:54 +0900

    conflict fix

--- 2774.cpp
+++ 2774.cpp
@@ -7,29 +7,17 @@ int main(){
 	cin>>T;
 	int ans[T];
 	for(int i = 0; i < T; i++){
-<<<<<<< HEAD
-		int X[T], R[11];
-		int temp = 0;
-		ans[i] = 0;
-		cin>>X[i];
-		for(int j = 0; j < 11; j++){
-=======
 		long long X[T], R[11];
 		int temp = 0;
 		ans[i] = 0;
 		cin>>X[i];
 		for(int j = 0; j < 10; j++){
->>>>>>> d420c373346c12f6500078a6c6a7fb8a7b18e678
 			R[j] = X[i]/pow(10,j);
 			if(R[j]==0){
 				break;
 			}
 		}
-<<<<<<< HEAD
-		for(int j = 0; j < 11; j++){
-=======
 		for(int j = 0; j < 10; j++){
->>>>>>> d420c373346c12f6500078a6c6a7fb8a7b18e678
 			R[j] = R[j] - R[j+1] * 10;
 			if(R[j+1]==0){
 				temp = j + 1;
@@ -42,11 +30,8 @@ int main(){
 				ans[i]++;
 			}
 		}
-<<<<<<< HEAD
-=======
 	}
 	for(int i = 0; i < T;i++){
->>>>>>> d420c373346c12f6500078a6c6a7fb8a7b18e678
 		cout<<ans[i]<<'\n';
 	}
 		
